d0f2b808b3ed78a7acc4564b44d6e4d636a233c9
zhuyong <zhu.yong7@zte.com.cn>
2014-12-06 19:33:22 +0300

    [Film Classify System]: add first test case without any implementation, it will fail

---
+++ lesson/src/main/scala/com/tw/workshop/tdd/FilmClassifySystem.scala
@@ -0,0 +1,8 @@
+package com.tw.workshop.tdd
+
+/**
+ * Created by root on 14-12-6.
+ */
+class FilmClassifySystem {
+
+}
---
+++ lesson/src/test/scala/com/tw/workshop/tdd/FilmClassifySystemTest.scala
@@ -0,0 +1,25 @@
+package com.tw.workshop.tdd
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{Matchers, FunSpec}
+
+/**
+ * Created by root on 12/6/14.
+ */
+@RunWith(classOf[JUnitRunner])
+class FilmClassifySystemTest extends FunSpec with Matchers {
+  describe("Film Classify System |") {
+
+    describe("Add Film |") {
+      it("should succeed when film is valid") {
+        val film = "The valid film name"
+        val filmSystem = new FilmClassifySystem()
+//        filmSystem.addFilm(film)
+        filmSystem.getFilm(film) should be(film)
+      }
+
+    }
+
+  }
+}
