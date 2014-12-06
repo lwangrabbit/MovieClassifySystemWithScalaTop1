21ad6027650159e92dfb5567cafb91fdc5819303
zhuyong <zhu.yong7@zte.com.cn>
2014-12-06 20:39:50 +0300

    [Film Classify System]: refator test code: extract prepare class to make test case more clear

--- lesson/src/test/scala/com/tw/workshop/tdd/FilmClassifySystemTest.scala
+++ lesson/src/test/scala/com/tw/workshop/tdd/FilmClassifySystemTest.scala
@@ -2,24 +2,35 @@ package com.tw.workshop.tdd
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.{Matchers, FunSpec}
+import org.scalatest.{BeforeAndAfterEach, Matchers, FunSpec}
 
 /**
  * Created by root on 12/6/14.
  */
 @RunWith(classOf[JUnitRunner])
-class FilmClassifySystemTest extends FunSpec with Matchers {
+class FilmClassifySystemTest extends FilmClassifySystemTestPrepare {
   describe("Film Classify System |") {
 
     describe("Add Film |") {
       it("should succeed when film is valid") {
         val film = "The film with valid name"
-        val filmSystem = new FilmClassifySystem()
-        filmSystem.addFilm(film)
-        filmSystem.getFilm(film).isDefined should be(true)
+        addFilm(film).getFilm(film).isDefined should be(true)
       }
 
     }
 
+  }
+}
+
+trait FilmClassifySystemTestPrepare extends FunSpec with Matchers {
+  var filmSystem = new FilmClassifySystem()
+
+  def addFilm(name: String) = {
+    filmSystem.addFilm(name)
+    this
+  }
+
+  def getFilm(name: String) = {
+    filmSystem.getFilm(name)
   }
 }
