4afa0d4082d62b878775dbaa658c886c799d6a78
zhuyong <zhu.yong7@zte.com.cn>
2014-12-06 22:36:17 +0300

    [Film Classify System]: add test case(invalid name) with implementation, it will succeed

--- lesson/src/main/scala/com/tw/workshop/tdd/FilmClassifySystem.scala
+++ lesson/src/main/scala/com/tw/workshop/tdd/FilmClassifySystem.scala
@@ -6,11 +6,16 @@ package com.tw.workshop.tdd
 class FilmClassifySystem {
   private var films: List[Film] = List()
 
-  def addFilm(filmName: String) {
-    films = new Film(filmName) :: films
+  def addFilm(name: String) {
+    if (isValidate(name)) {
+      films = new Film(name) :: films
+    }
   }
 
   def getFilmByName(name: String) = films.find(f => f.filmName == name)
+
+  private def isValidate(name: String) = name == """[a-zA-Z0-9 ]+""".r.findFirstIn(name).getOrElse("")
+
 
 }
 
