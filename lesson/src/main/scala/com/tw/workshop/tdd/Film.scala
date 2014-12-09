330b9e380b643a457954632b1e0afde382acc631
zhuyong <zhu.yong7@zte.com.cn>
2014-12-09 17:04:53 +0300

    [Film Classify System]: refator : remove magic number

--- lesson/src/main/scala/com/tw/workshop/tdd/Film.scala
+++ lesson/src/main/scala/com/tw/workshop/tdd/Film.scala
@@ -4,7 +4,7 @@ package com.tw.workshop.tdd
  * Created by root on 14-12-6.
  */
 class Film(var name: String, var category: String) {
-  var score: Int = 0
+  var score: Int = ScoreCfg.defaultUnScore
 
   def updateName(modName: String) =  { name = modName; this }
   def updateCategory(modCategory: String) = { category = modCategory; this }
